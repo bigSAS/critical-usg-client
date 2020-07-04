18de165449227b70c62d71bc757e8eaa544b1e7f
bigSAS <0980_GitHub>
2020-07-04 19:30:48 +0200

    test

---
+++ src/components/Foo.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-circular-progress
+      indeterminate
+      size="90px"
+      :thickness="0.2"
+      color="lime"
+      center-color="grey-8"
+      track-color="transparent"
+      class="q-ma-md"
+    />
+</template>
+
+<script>
+    export default {
+        name: "Foo"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,19 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+<!--    <img-->
+<!--      alt="Quasar logo"-->
+<!--      src="~assets/quasar-logo-full.svg"-->
+<!--    >-->
+    <Foo/>
   </q-page>
 </template>
 
 <script>
+  import Foo from '../components/Foo'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Foo
+  }
 }
 </script>
